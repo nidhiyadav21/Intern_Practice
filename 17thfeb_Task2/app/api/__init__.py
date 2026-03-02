72c85f48373d0166767469753abfddbfe93b90fc
nidhiyadav21 <nidhiyadavv41@gmail.com>
2026-03-02 11:05:15 +0530

    Rename folder name

--- 17thfeb_Task2/app/api/__init__.py
+++
@@ -1,2 +0,0 @@
-#api package - It contains the endpoints (URL paths)
-#transactions.py & categories.py: These define your GET, POST, DELETE, etc., operations. They handle the incoming requests and return responses.
--- 17thfeb_Task2/app/core/config.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    MONGODB_URL: str = 'mongodb://localhost:27017'
-    DB_NAME: str = 'finance_tracker'
-settings = Settings()
