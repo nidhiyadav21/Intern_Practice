046c6d82e053c387a70b9c99763a165c83c6578b
nidhiyadav21 <nidhiyadavv41@gmail.com>
2026-03-11 11:28:10 +0530

    Added comments for better understanding

--- 10-03-2026_Kmeans/kmeans_clustering.py
+++ 10-03-2026_Kmeans/kmeans_clustering.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.compose import  make_column_selector, make_column_transformer
@@ -14,28 +13,18 @@ class CustomerSegmentation:
        A class to perform end-to-end Customer Segmentation using K-Means clustering.
        Includes data loading, preprocessing, pipeline building, and visualization.
     """
-    def __init__(self,file_path,test_size = 0.3,random_state=42):
+    def __init__(self,file_path,random_state=42):
         """
                 Initializes the CustomerSegmentation class with file path and model parameters.
 
                 Args:
                     file_path (str): Path to the CSV dataset.
-                    test_size (float): Proportion of the dataset to include in the test split.
                     random_state (int): Seed used by the random number generator for reproducibility.
         """
         self.file_path = file_path
-        self.test_size = test_size
         self.random_state = random_state
 
         self.df = None
-
-        self.X = None
-        self.y = None
-
-        self.X_train = None
-        self.y_train = None
-        self.X_test = None
-        self.y_test = None
 
         self.pipeline = None
 
@@ -71,15 +60,15 @@ class CustomerSegmentation:
 
             for col_name in numeric_col_list:
                 # We use .copy().dropna() to ensure we have a clean Series of numbers
-                clean_series = self.df[col_name].dropna()
+                clean_series = self.df[col_name].dropna()  # Creates a copy of the column and removes any NaN (null) values.
 
                 # Check if the series has any data points to plot
-                if len(clean_series) > 0:
+                if len(clean_series) > 0:            #A safety check to ensure there is actually data left to plot after dropping nulls.
                     plt.figure(figsize=(6, 4))
                     plt.boxplot(clean_series)
                     plt.title(f"Boxplot of {col_name}")
                     plt.xlabel(col_name)
-                    plt.grid(True)
+                    plt.grid(True)                   #Adds a background grid
                     plt.show()
                 else:
                     print(f"Skipping {col_name}: No numeric data found.")
@@ -99,14 +88,14 @@ class CustomerSegmentation:
                 # Capping outliers to the 99th percentile
                 for col in ['Work_Experience', 'Family_Size']:
                     upper_limit = self.df[col].quantile(0.99)
-                    self.df[col] = np.where(self.df[col] > upper_limit, upper_limit, self.df[col])
+                    self.df[col] = np.where(self.df[col] > upper_limit, upper_limit, self.df[col])   # This is capping - If a value is higher than the 99th percentile, it is reset to that limit
 
                 self.df.drop(columns=['ID', 'Profession', 'Var_1'], inplace=True, errors='ignore')
 
                 # 1. Map Ordinal Features (CRITICAL for K-Means)
-                res_map = {'Low': 1, 'Average': 2, 'High': 3}
+                res_map = {'Low': 1, 'Average': 2, 'High': 3}         #Defines a dictionary to convert text-based ratings into meaningful numbers.
                 if 'Spending_Score' in self.df.columns:
-                    self.df['Spending_Score'] = self.df['Spending_Score'].map(res_map)
+                    self.df['Spending_Score'] = self.df['Spending_Score'].map(res_map)  #This is Ordinal Encoding
 
                 # 2. Handle missing values
                 num_cols = self.df.select_dtypes(include=[np.number]).columns
@@ -143,8 +132,8 @@ class CustomerSegmentation:
                 (OneHotEncoder(handle_unknown='ignore', drop='if_binary'),cat_selector),
                 (StandardScaler(), num_selector),
             )
-
-            self.pipeline = make_pipeline(transformer,PCA(n_components=0.95), KMeans(n_clusters=k, random_state=self.random_state, n_init=10))
+            kmeans = KMeans(n_clusters=k, random_state=self.random_state, n_init=10) #runs the algorithm 10 times with different starting points and picks the best one
+            self.pipeline = make_pipeline(transformer,PCA(n_components=0.95), kmeans) #Reduces data complexity by keeping only the most important features that capture 95% of the information.
             print(f"Pipeline built with PCA and k={k}")
         except Exception as e:
             print(f"An unexcepted error occured while building pipeline: {e} ")
@@ -172,7 +161,7 @@ class CustomerSegmentation:
                 inertia.append(km.inertia_)
 
             plt.figure(figsize=(8, 4))
-            plt.plot(K, inertia, 'bx-')
+            plt.plot(K, inertia, 'bx-')  #inertia-Sum of Squared Distances
             plt.title('Elbow Method')
             plt.show()
         except Exception as e:
@@ -187,7 +176,7 @@ class CustomerSegmentation:
         self.find_optimal_k()
         try:
             print("Training data...")
-            self.pipeline.fit(self.df)
+            self.pipeline.fit(self.df)  #It sends the raw data through the entire pipeline created earlier (Mapping->Scaling->PCA-> K-Means). The model "learns" the patterns and finds the best center point for each group.
             print("Training Completed")
         except Exception as e:
             print(f"An unexcepted error occured while training data: {e} ")
@@ -201,44 +190,18 @@ class CustomerSegmentation:
         self.train_data()
         try:
             print("Evaluating model...")
-            X_transformed = self.pipeline.named_steps['columntransformer'].transform(self.df)
+            X_transformed = self.pipeline.named_steps['columntransformer'].transform(self.df) #This takes the original data and processes it (scaling/encoding) exactly how the model expects it
             labels = self.pipeline.named_steps['kmeans'].labels_
 
             score = silhouette_score(X_transformed, labels)
             print(f"--- Optimized Silhouette Score: {score:.4f} ---")
-            self.df['Cluster'] = labels
+            self.df['Cluster'] = labels    #Create a new column in the data to show each customer's group
+            print(self.df.head())
             print("Evaluation Completed")
         except Exception as e:
             print(f"An unexcepted error occured while evaluating model: {e} ")
 
 
-
-    def describe_clusters(self):
-        """
-                Generates and prints cluster profiles based on the mean of numeric features.
-        """
-
-        self.evaluate_model()
-        try:
-            print("\n--- Generating Cluster Profiles ---")
-
-            # 1. Filter for numeric types only (including the Cluster labels)
-            # Using 'number' covers ints, floats, and  mapped Spending_Score
-            numeric_df = self.df.select_dtypes(include=[np.number])
-
-            # 2. Calculate averages
-            analysis = numeric_df.groupby('Cluster').mean().reset_index()
-
-            print("\n--- Cluster Averages ---")
-            print(analysis)
-
-            # 3. Visualization
-            plt.figure(figsize=(8, 5))
-            sns.barplot(x='Cluster', y='Age', data=analysis, palette='viridis', hue='Cluster', legend=False)
-            plt.title("Average Age per Cluster")
-            plt.show()
-        except Exception as e:
-            print(f"An unexcepted error occured while generating cluster profiles: {e} ")
 
 
     def visualize(self):
@@ -246,7 +209,7 @@ class CustomerSegmentation:
              Visualizes clusters in a 2D space using the PCA components.
         """
 
-        self.describe_clusters()
+        self.evaluate_model()
         try:
             print("Visualizing Clusters...")
             # Get the PCA coordinates
