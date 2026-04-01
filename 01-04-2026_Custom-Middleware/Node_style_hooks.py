14255328ecd0e785895d45264c1e5cf338fdf844
nidhiyadav21 <nidhiyadavv41@gmail.com>
2026-04-01 17:56:23 +0530

    Implemented Custom Middleware

---
+++ 01-04-2026_Custom-Middleware/Node_style_hooks.py
@@ -0,0 +1,54 @@
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+from langchain.agents.middleware import before_model, after_model, AgentState
+from langchain.messages import AIMessage,HumanMessage
+from langgraph.runtime import Runtime
+from typing import Any
+
+@before_model(can_jump_to=["end"])
+def check_message_limit(state: AgentState, runtime: Runtime) -> dict[str, Any] | None:
+    if len(state["messages"]) >= 6:
+        return {
+            "messages": [AIMessage("Conversation limit reached.")],
+            "jump_to": "end"
+        }
+    return None
+
+@after_model
+def log_response(state: AgentState, runtime: Runtime) -> dict[str, Any] | None:
+
+    return None
+
+llm = ChatOllama(model="llama3.2:1b")
+
+agent = create_agent(
+    llm,
+    tools=[],
+    middleware=[check_message_limit, log_response]  #CRITICAL STEP
+)
+
+history = []
+
+print("--- Chat Started (Limit: 6 messages total) ---")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() in ["exit", "quit"]:
+        break
+
+    # Add user message to history
+    history.append(HumanMessage(content=user_input))
+
+    # Invoke agent with the full history
+    response = agent.invoke({"messages": history})
+
+    # Update history with the AI's response
+    ai_msg = response["messages"][-1]
+    history.append(ai_msg)
+
+    print(f"AI: {ai_msg.content}")
+
+    # Break the loop if the limit message was sent
+    if "Conversation limit reached" in ai_msg.content:
+        print("Session Ended")
+        break
+
